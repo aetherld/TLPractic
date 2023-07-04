90d67c9d8e21c5771125641b944baf228e4f8417
aetherld <124870915+aetherld@users.noreply.github.com>
2023-07-04 20:02:02 +0300

    Add files via upload

---
+++ bubblesort.cs
@@ -0,0 +1,19 @@
+﻿int[] number = { 3, 5, 1, -10, 18, 4, 2, 8 };
+int temp;
+
+for (int i = 0; i < number.Length; i++)
+{
+    for (int j = 0; j < number.Length - 1 - i; j++)
+    {
+        if (number[j] > number[ j +1 ])
+        {
+            temp = number[j];
+            number[j] = number[j + 1];
+            number[j + 1] = temp;
+        }
+    }
+}
+for (int i = 0; i < number.Length; i++)
+{
+    Console.WriteLine(number[i]);
+}
---
+++ info.cs
@@ -0,0 +1,13 @@
+﻿var name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+var email = Console.ReadLine();
+var gitname = Console.ReadLine();
+
+Console.WriteLine("Личная информация:");
+
+
+
+Console.WriteLine("\tИмя:" + name);
+Console.WriteLine("\tВозраст:" + age);
+Console.WriteLine("\tЭлектронная почта:" + email);
+Console.WriteLine("\tГитхаб:" +  gitname); 
